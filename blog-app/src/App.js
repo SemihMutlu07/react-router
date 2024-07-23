594d7e3ec20195b2bc3a006d8308673b27b2b363
SemihMutlu07 <117535331+SemihMutlu07@users.noreply.github.com>
2024-07-23 22:19:01 +0300

    yea

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '/.pages/Home';
+import Login from '/.pages/Login';
+import Register from '/.pages/Register';
+import CreatePost from '/.pages/CreatePoste';
+import EditPost from '/.pages/EditPost';
+import PostList from '/.pages/PostList';
+import PostDetail from '/.pages/PostDetail';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path='/login' component={Login}/>
+        <Route path='/register' component={Register}/>
+        <Route path='/create' component={CreatePost}/>
+        <Route path='/edit/:id' component={EditPost}/>
+        <Route path='/posts' component={PostList}/>
+        <Route path='/post/:id' component={PostDetail}/>
+      </Switch>
+    </Router>
   );
 }
 
